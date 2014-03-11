b1063674d60b96c570adbf38cd009e45fbf1d049
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-03-10 22:44:30 -0400

    Adding DatabaseChannel storage

---
+++ app/models/harbinger/database_channel_message.rb
@@ -0,0 +1,9 @@
+module Harbinger
+  class DatabaseChannelMessage < ActiveRecord::Base
+    self.table_name = 'harbinger_messages'
+    has_many :elements, class_name: 'Harbinger::DatabaseChannelMessageElement', foreign_key: :message_id
+
+    def self.store_message(message)
+    end
+  end
+end
