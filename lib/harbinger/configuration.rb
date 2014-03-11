fb99874df03c73fae4598d8fd613ca4f40f89a84
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-03-10 20:58:39 -0400

    Lazy loading of logger and database storage
    Since these are configuration options, no sense attempting to
    initialize them.

--- lib/harbinger/configuration.rb
+++ lib/harbinger/configuration.rb
@@ -2,12 +2,23 @@ require 'harbinger/exceptions'
 module Harbinger
   class Configuration
 
-    def initialize(collaborators = {})
-      self.logger = collaborators.fetch(:logger) { default_logger }
-      self.database_storage = collaborators.fetch(:database_storage) { default_database_storage }
+    def logger
+      @logger || default_logger
     end
 
-    attr_reader :logger
+    def logger=(object)
+      raise ConfigurationError.new("Expected Harbinger.database_storage to respond_to #add. #{object.inspect} does not respond to #add") unless object.respond_to?(:add)
+      @logger = object
+    end
+
+    def database_storage
+      @database_storage || default_database_storage
+    end
+
+    def database_storage=(object)
+      raise ConfigurationError.new("Expected Harbinger.database_storage to respond_to #store_message. #{object.inspect} does not respond to #add") unless object.respond_to?(:store_message)
+      @database_storage = object
+    end
 
     def default_logger
       if defined?(Rails)
@@ -19,14 +30,6 @@ module Harbinger
     end
     private :default_logger
 
-    def logger=(object)
-      raise ConfigurationError.new("Expected Harbinger.database_storage to respond_to #add. #{object.inspect} does not respond to #add") unless object.respond_to?(:add)
-      @logger = object
-    end
-    protected :logger=
-
-    attr_reader :database_storage
-
     def default_database_storage
       Class.new do
         def self.store_message(message)
@@ -35,11 +38,6 @@ module Harbinger
     end
     private :default_database_storage
 
-    def database_storage=(object)
-      raise ConfigurationError.new("Expected Harbinger.database_storage to respond_to #store_message. #{object.inspect} does not respond to #add") unless object.respond_to?(:store_message)
-      @database_storage = object
-    end
-    protected :database_storage=
 
   end
 end
--- spec/lib/harbinger/configuration_spec.rb
+++ spec/lib/harbinger/configuration_spec.rb
@@ -3,21 +3,38 @@ require 'harbinger/configuration'
 
 module Harbinger
   describe Configuration do
+    Given(:configuration) { described_class.new }
     context '#logger' do
       context 'interface' do
-        Given(:configuration) { described_class.new }
         Then { expect(configuration.logger).to respond_to :add }
       end
 
       context 'override with valid logger' do
         Given(:logger) { double('Logger', add: true) }
-        Given(:configuration) { described_class.new(logger: logger) }
+        When { configuration.logger = logger }
         Then { expect(configuration.logger).to eq(logger) }
       end
 
       context 'override without valid logger' do
         Given(:logger) { double('Logger') }
-        Then { expect { described_class.new(logger: logger) }.to raise_error(ConfigurationError) }
+        Then { expect { configuration.logger = logger }.to raise_error(ConfigurationError) }
+      end
+    end
+
+    context '#database_storage' do
+      context 'interface' do
+        Then { expect(configuration.database_storage).to respond_to :store_message }
+      end
+
+      context 'override with valid database_storage' do
+        Given(:database_storage) { double('Database Storage', store_message: true) }
+        When { configuration.database_storage = database_storage }
+        Then { expect(configuration.database_storage).to eq(database_storage) }
+      end
+
+      context 'override without valid database_storage' do
+        Given(:database_storage) { double('Database Storage') }
+        Then { expect { configuration.database_storage = database_storage }.to raise_error(ConfigurationError) }
       end
     end
   end
