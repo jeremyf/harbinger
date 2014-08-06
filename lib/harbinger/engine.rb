8d93dbc9c93713a88a4e760352c97608b4025648
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-08-06 14:30:47 -0400

    Ensuring the engine loads appropriate deps
    By putting this in the to_prepare, all of the gems are loaded and
    required.
    
    This may create issues with implementation. So be wary!

--- lib/harbinger/engine.rb
+++ lib/harbinger/engine.rb
@@ -1,5 +1,12 @@
 module Harbinger
   class Engine < ::Rails::Engine
     engine_name 'harbinger'
+
+    config.to_prepare do
+      # Because I don't want to auto-require all of the dependent channels
+      Harbinger.default_channels.each do |channel_name|
+        require "harbinger/channels/#{channel_name}_channel"
+      end
+    end
   end
 end
